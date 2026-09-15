39bbfc6cb95918160aabd9a646992ac954705074
mehrshad <mehrshadkhakifirooz@gmail.com>
2026-09-15 15:01:11 +0330

    add shop context

---
+++ LampShade/ShopManagement.Infrastructure.EFCore/ShopContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EFCore.Mapping;
+
+namespace ShopManagement.Infrastructure.EFCore
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var Assembly = typeof(ProductCategoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
