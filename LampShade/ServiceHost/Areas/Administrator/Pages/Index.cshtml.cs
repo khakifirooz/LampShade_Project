997ab56a2af081ec81fe57e9202bd327abf0b105
mehrshad <mehrshadkhakifirooz@gmail.com>
2026-09-20 14:56:51 +0330

    add host

---
+++ LampShade/ServiceHost/Areas/Administrator/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administrator
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- LampShade/ServiceHost/Program.cs
+++ LampShade/ServiceHost/Program.cs
@@ -32,4 +32,6 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+app.MapDefaultControllerRoute();
+
 app.Run();
